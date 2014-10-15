1f81d10ed03160fb2b676c3109eadfe767562f5c
Αλέξανδρος Τσιγγίλης <alextsigilis@gmail.com>
2014-10-15 20:00:42 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,9 +1,14 @@
-import webapp2 , handler
+import webapp2 , handler, models
 
 #Add your views here
 class MainPage(handler.Handler):
 	def get(self):
-		self.write('Hello Wolrd!!!')
+		user_id = self.get_cookie('user_id')
+		if user_id: 
+			user = models.User.get_by_id(int(user_id))
+			self.write('Hello, %s'%user.username)
+		else:
+			self.write('Hello you can <a href="/login">login</a>.')
 
 class Login(handler.Handler):
 	def get(self):
